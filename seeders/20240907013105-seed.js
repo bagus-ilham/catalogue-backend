4ffc0ed3991016844647e67b3f93f45458f3a60b
User <bagusilham74@gmail.com>
2024-09-13 16:33:39 +0700

    finish db name and subname

--- seeders/20240907013105-seed.js
+++ seeders/20240907013105-seed.js
@@ -1,8 +1,9 @@
 'use strict';
 const category = require('../data/category.json');
 const product = require('../data/hijabs.json');
-// const product2 = require('../data/hijabs2.json')
+const product2 = require('../data/hijabs2.json')
 const color = require('../data/colors.json')
+const color2 = require('../data/colors2.json')
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
@@ -18,12 +19,24 @@ module.exports = {
       return el;
     })
     await queryInterface.bulkInsert('Products', dataProduct, {});
+
+    const dataProduct2 = product2.map((el) => {
+      el.createdAt = el.updatedAt = new Date();
+      return el;
+    })
+    await queryInterface.bulkInsert('Products', dataProduct2, {});
     
     const dataColor = color.map((el) => {
       el.createdAt = el.updatedAt = new Date();
       return el;
     })
     await queryInterface.bulkInsert('Colors', dataColor, {});
+
+    const dataColor2 = color2.map((el) => {
+      el.createdAt = el.updatedAt = new Date();
+      return el;
+    })
+    await queryInterface.bulkInsert('Colors', dataColor2, {});
   },
 
   async down (queryInterface, Sequelize) {
